f487a6b6bb6c3d02ff3551524c4a10ef9ceedda6
Jori <jori.winderickx@kuleuven.be>
2017-03-17 15:48:31 +0100

    Resetting because reason

--- lora-nodeOTP/include/board.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BOARD_H__
-#define __BOARD_H__
-
-#define USE_BAND_868
-
-#endif //__BOARD_H__
--- lora-nodeOTP/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "mbed.h"
-#include "board.h"
-#include "LoRaMac.h"
-
-void main(){
-    
-}
