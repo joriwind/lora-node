eee5fdd52c1e2d45500dbc2f47689a9d57810fda
Jori <jori.winderickx@kuleuven.be>
2017-04-11 15:44:21 +0200

    Configuration for joining network & indicator

--- lora-nodeOTP/include/comissioning.h
+++ lora-nodeOTP/include/comissioning.h
@@ -17,5 +17,33 @@
  */
 #define LORAWAN_APP_KEY     { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C }
 
+/**  Channel definition   **/
+/*!
+ * LoRaWAN Adaptive Data Rate
+ *
+ * \remark Please note that when ADR is enabled the end-device should be static
+ */
+#define LORAWAN_ADR_ON                              1
+
+/*!
+ * LoRaWAN ETSI duty cycle control enable/disable
+ *
+ * \remark Please note that ETSI mandates duty cycled transmissions. Use only for test purposes
+ */
+#define LORAWAN_DUTYCYCLE_ON                        true
+
+#define USE_SEMTECH_DEFAULT_CHANNEL_LINEUP          1
+
+#if( USE_SEMTECH_DEFAULT_CHANNEL_LINEUP == 1 ) 
+
+#define LC4                { 867100000, { ( ( DR_5 << 4 ) | DR_0 ) }, 0 }
+#define LC5                { 867300000, { ( ( DR_5 << 4 ) | DR_0 ) }, 0 }
+#define LC6                { 867500000, { ( ( DR_5 << 4 ) | DR_0 ) }, 0 }
+#define LC7                { 867700000, { ( ( DR_5 << 4 ) | DR_0 ) }, 0 }
+#define LC8                { 867900000, { ( ( DR_5 << 4 ) | DR_0 ) }, 0 }
+#define LC9                { 868800000, { ( ( DR_7 << 4 ) | DR_7 ) }, 2 }
+#define LC10               { 868300000, { ( ( DR_6 << 4 ) | DR_6 ) }, 1 }
+
+#endif
 
 #endif //__LORA_COMMISSIONING_H__
--- lora-nodeOTP/src/main.cpp
+++ lora-nodeOTP/src/main.cpp
@@ -14,19 +14,21 @@ static void MlmeConfirm( MlmeConfirm_t *MlmeConfirm );
 LoRaMacPrimitives_t LoRaMacPrimitives;
 LoRaMacCallback_t LoRaMacCallbacks;
 LoRaMacStatus_t Status;
-
-
 static uint8_t DevEui[] = LORAWAN_DEV_EUI;
 static uint8_t AppEui[] = LORAWAN_APP_EUI;
 static uint8_t AppKey[] = LORAWAN_APP_KEY;
 
+//Indication led, indicates if joined a network
+DigitalOut ledConnected(LED1);
+
+
 //Control function of program
 int main( void ){
-    MlmeReq_t mlmeReq;
     LoRaMacStatus_t status;
 
     //Board initilization
     BoardInit();
+    ledConnected = 0;
 
     //LoRaWAN MAC layer initialisation
     LoRaMacPrimitives.MacMcpsConfirm = McpsConfirm;
@@ -37,9 +39,37 @@ int main( void ){
     if( Status == LORAMAC_STATUS_OK )
     {
         // Initialization successful
+    }else{
+        return -1;
     }
+    mibReq.Type = MIB_ADR;
+    mibReq.Param.AdrEnable = LORAWAN_ADR_ON;
+    LoRaMacMibSetRequestConfirm( &mibReq );
+
+    mibReq.Type = MIB_PUBLIC_NETWORK;
+    mibReq.Param.EnablePublicNetwork = LORAWAN_PUBLIC_NETWORK;
+    LoRaMacMibSetRequestConfirm( &mibReq );
+
+#if defined( USE_BAND_868 )
+    LoRaMacTestSetDutyCycleOn( LORAWAN_DUTYCYCLE_ON );
+
+#if( USE_SEMTECH_DEFAULT_CHANNEL_LINEUP == 1 ) 
+    LoRaMacChannelAdd( 3, ( ChannelParams_t )LC4 );
+    LoRaMacChannelAdd( 4, ( ChannelParams_t )LC5 );
+    LoRaMacChannelAdd( 5, ( ChannelParams_t )LC6 );
+    LoRaMacChannelAdd( 6, ( ChannelParams_t )LC7 );
+    LoRaMacChannelAdd( 7, ( ChannelParams_t )LC8 );
+    LoRaMacChannelAdd( 8, ( ChannelParams_t )LC9 );
+    LoRaMacChannelAdd( 9, ( ChannelParams_t )LC10 );
+
+    mibReq.Type = MIB_RX2_CHANNEL;
+    mibReq.Param.Rx2Channel = ( Rx2ChannelParams_t ){ 869525000, DR_3 };
+    LoRaMacMibSetRequestConfirm( &mibReq );
+#endif
+
 
     //LoRaWAN over-the-air activation
+    MlmeReq_t mlmeReq;
     // Setup the request type
     mlmeReq.Type = MLME_JOIN;
     // Fill the join parameters
@@ -52,6 +82,12 @@ int main( void ){
     if( status == LORAMAC_STATUS_OK )
     {
         // Join request was send successfully
+    }
+
+    //Processing loop
+    while(1)
+    {
+        //Waiting for requests
     }
 }
 
@@ -76,6 +112,7 @@ static void MlmeConfirm( MlmeConfirm_t *MlmeConfirm )
             case MLME_JOIN:
             {
                 // Status is OK, node has joined the network
+                ledConnected = 1;
                 break;
             }
             case MLME_LINK_CHECK:
