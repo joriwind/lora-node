de4a961cc53171342a1faca28f5c9f224957ca76
Jori <jori.winderickx@kuleuven.be>
2017-04-11 12:56:33 +0200

    LoRaWAN init & over-air activation, base code

--- lora-nodeOTP/include/board.h
+++ lora-nodeOTP/include/board.h
@@ -1,6 +1,26 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
 
+#include "mbed.h"
+#include "system/timer.h"
+#include "debug.h"
+#include "system/utilities.h"
+#include "sx1272-hal.h"
+
 #define USE_BAND_868
+
+extern SX1272MB2xAS Radio;
+
+/*!
+ * \brief Initializes the target board peripherals.
+ */
+void BoardInit( void );
+
+/*!
+ * \brief Measure the Battery level
+ *
+ * \retval value  battery level ( 0: very low, 254: fully charged )
+ */
+uint8_t BoardGetBatteryLevel( void );
 
 #endif //__BOARD_H__
--- lora-nodeOTP/src/main.cpp
+++ lora-nodeOTP/src/main.cpp
@@ -2,6 +2,86 @@
 #include "board.h"
 #include "LoRaMac.h"
 
-void main(){
-    
+/* Callback functions for LoRaWAN-lib */
+static void McpsConfirm( McpsConfirm_t *McpsConfirm );
+static void McpsIndication( McpsIndication_t *McpsIndication );
+static void MlmeConfirm( MlmeConfirm_t *MlmeConfirm );
+
+/*
+ * Configuration variables for LoRaWAN-lib
+ */
+LoRaMacPrimitives_t LoRaMacPrimitives;
+LoRaMacCallback_t LoRaMacCallbacks;
+LoRaMacStatus_t Status;
+
+
+static uint8_t DevEui[] = LORAWAN_DEV_EUI;
+static uint8_t AppEui[] = LORAWAN_APP_EUI;
+static uint8_t AppKey[] = LORAWAN_APP_KEY;
+
+//Control function of program
+int main( void ){
+    MlmeReq_t mlmeReq;
+    LoRaMacStatus_t status;
+
+    //LoRaWAN MAC layer initialisation
+    LoRaMacPrimitives.MacMcpsConfirm = McpsConfirm;
+    LoRaMacPrimitives.MacMcpsIndication = McpsIndication;
+    LoRaMacPrimitives.MacMlmeConfirm = MlmeConfirm;
+    LoRaMacCallbacks.GetBatteryLevel = BoardGetBatteryLevel;
+    Status = LoRaMacInitialization( &LoRaMacPrimitives, &LoRaMacCallbacks );
+    if( Status == LORAMAC_STATUS_OK )
+    {
+        // Initialization successful
+    }
+
+    //LoRaWAN over-the-air activation
+    // Setup the request type
+    mlmeReq.Type = MLME_JOIN;
+    // Fill the join parameters
+    mlmeReq.Req.Join.DevEui = DevEui;
+    mlmeReq.Req.Join.AppEui = AppEui;
+    mlmeReq.Req.Join.AppKey = AppKey;
+    // Add the number of trials for the join request
+    mlmeReq.Req.Join.NbTrials = 3;
+    status = LoRaMacMlmeRequest( &mlmeReq );
+    if( status == LORAMAC_STATUS_OK )
+    {
+        // Join request was send successfully
+    }
+}
+
+
+static void McpsConfirm( McpsConfirm_t *McpsConfirm )
+{
+  // Implementation of the MCPS-Confirm primitive
+}
+
+static void McpsIndication( McpsIndication_t *McpsIndication )
+{
+  // Implementation of the MCPS-Indication primitive
+}
+
+static void MlmeConfirm( MlmeConfirm_t *MlmeConfirm )
+{
+  // Implementation of the MLME-Confirm primitive
+  if( MlmeConfirm->Status == LORAMAC_EVENT_INFO_STATUS_OK )
+    {
+        switch( MlmeConfirm->MlmeRequest )
+        {
+            case MLME_JOIN:
+            {
+                // Status is OK, node has joined the network
+                break;
+            }
+            case MLME_LINK_CHECK:
+            {
+                // Check DemodMargin
+                // Check NbGateways
+                break;
+            }
+            default:
+                break;
+        }
+    }
 }
